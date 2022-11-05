e59a15bfd7f91cb72cc9f73a4d879e1337c7b70a
Sunitastha <sunitashrestha2057@gmail.com>
2022-11-05 22:07:24 +0545

    <update>

--- script.js
+++ script.js
@@ -41,3 +41,31 @@ lists.addEventListener("click",(event)=>{
 
     }; 
 })
+//update list
+
+lists.forEach(element => {
+    element.addEventListener("click",(event)=>{
+    if(event.target.className=="edit"){
+        const li= event.target.parentElement;
+        const p = li.querySelector("p");
+        const input = document.createElement("input");
+        input.type="text";
+        input.value=p.innerText;
+        li.insertBefore(input,p);
+        li.removeChild(p);
+        event.target.innerText="save";
+        event.target.className="save";
+}
+else if(event.target.className === "save"){
+            const li = event.target.parentElement;
+            const input = li.querySelector("input[type='text']");
+            const p = document.createElement("p");
+            p.innerText=input.value;
+            p.className="title";
+            li.insertBefore(p,input);
+            li.removeChild(input);
+            event.target.innerText="update";
+            event.target.className="edit";
+}
+})
+})
